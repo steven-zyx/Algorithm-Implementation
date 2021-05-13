f79192494a4d4ee819000ab59d8622a9d0253161
Yuxuan Zhu <1078813195@qq.com>
2021-05-13 10:26:35 +0800

    1

---
+++ BasicDataStrcture/List1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BasicDataStrcture
+{
+    public class List1<T>
+    {
+        private T[] _data;
+        private int _current;
+
+        public List1()
+        {
+            _data = new T[4];
+        }
+
+        public void Add(T t)
+        {
+            if (_current == _data.Length)
+                Resize();
+
+            _data[_current] = t;
+            _current++;
+        }
+
+        public int Count
+        {
+            get { return _current; }
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i >= _current)
+                    throw new Exception("index out of range");
+                else
+                    return _data[i];
+            }
+            set
+            {
+                if (i >= _current)
+                    throw new Exception("index out of range");
+                else
+                    _data[i] = value;
+            }
+        }
+
+        public void Resize()
+        {
+            T[] newData = new T[_data.Length * 2];
+            _data.CopyTo(newData, 0);
+            _data = newData;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using BasicDataStrcture;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestListResize();
+            Console.ReadKey();
+        }
+
+        public static void TestListResize()
+        {
+            List1<int> numbers = new List1<int>();
+            Stopwatch watch = new Stopwatch();
+            for (int i = 0; i < 1000; i++)
+            {
+                watch.Restart();
+                numbers.Add(i);
+                Console.WriteLine(watch.ElapsedTicks);
+            }
+        }
+    }
+}
