d4db9cfe17baaa814ff144a7834be96331447bae
Yuxuan Zhu <1078813195@qq.com>
2021-09-26 09:01:37 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -57,7 +57,8 @@ namespace ConsoleApp1
             //ShowRandomPhoneNumbers();
             //SubStringMatches();
             //ShowCyclicRotation();
-            TandemRepeatSearch();
+            //TandemRepeatSearch();
+            TestGoTo();
             Console.ReadLine();
         }
 
@@ -779,6 +780,22 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine($"start index:{longest.index}\trepeat:{longest.count}");
             }
+        }
+
+        public static void TestGoTo()
+        {
+            string txt = "AABAAA";
+
+            int i = -1;
+            sm: i++;
+            s0: if (txt[i] != 'A') goto sm;
+            s1: if (txt[i] != 'A') goto s0;
+            s2: if (txt[i] != 'B') goto s0;
+            s3: if (txt[i] != 'A') goto s2;
+            s4: if (txt[i] != 'A') goto s0;
+            s5: if (txt[i] != 'A') goto s3;
+
+            Console.WriteLine(i - 8);
         }
     }
 }
---
+++ String/RabinKarp2D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    public class RabinKarp2D
+    {
+        public RabinKarp2D(string[,] pattern)
+        {
+
+        }
+
+        public void ComputeHash(string[,] pattern)
+        {
+            //for(int i =0;i<pattern.GetLength())
+        }
+    }
+}
