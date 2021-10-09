471198c4d8f4b466e0fac86362a70a475b351aed
Yuxuan Zhu <1078813195@qq.com>
2021-10-09 16:36:47 +0800

    weighted graph data strucutre

---
+++ Graph/Edge.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmImplementation.Graph
+{
+    public class Edge : IComparable<Edge>
+    {
+        protected int _v;
+        protected int _w;
+
+        public double Weight { get; }
+
+        public Edge(int v, int w, double weight)
+        {
+            _v = v;
+            _w = w;
+            Weight = weight;
+        }
+
+        public int Either() => _v;
+
+        public int Other(int v)
+        {
+            if (v == _v) return _w;
+            else if (v == _w) return _v;
+            else
+                throw new InvalidOperationException();
+        }
+
+        public int CompareTo(Edge other)
+        {
+            if (Weight < other.Weight)
+                return -1;
+            else if (Weight > other.Weight)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Graph/EdgeWeightedGraph.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicDataStrcture;
+
+namespace AlgorithmImplementation.Graph
+{
+    public class EdgeWeightedGraph
+    {
+        protected Bag_L<Edge>[] _adjacencyList;
+
+        public int V { get; }
+
+        public int E { get; protected set; }
+
+        protected EdgeWeightedGraph(int v)
+        {
+            V = v;
+            _adjacencyList = new Bag_L<Edge>[V];
+            for (int i = 0; i < v; i++)
+                _adjacencyList[i] = new Bag_L<Edge>();
+        }
+
+        public EdgeWeightedGraph(int v, IEnumerable<(int, int, double)> edges) : this(v)
+        {
+            foreach (var edge in edges)
+                AddEdge(edge.Item1, edge.Item2, edge.Item3);
+        }
+
+        public void AddEdge(int v, int w, double weight)
+        {
+            Edge e = new Edge(v, w, weight);
+            _adjacencyList[v].Add(e);
+            _adjacencyList[w].Add(e);
+            E++;
+        }
+
+        public IEnumerable<Edge> Adj(int v) => _adjacencyList[v];
+
+        public IEnumerable<Edge> Edges()
+        {
+            for (int i = 0; i < V; i++)
+                foreach (Edge e in _adjacencyList[i])
+                    if (i < e.Other(i))
+                        yield return e;
+        }
+
+    }
+}
