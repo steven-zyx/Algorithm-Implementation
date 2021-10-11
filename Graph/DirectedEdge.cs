1617e873bfa3adeff326149371a10d4ca77607cd
Yuxuan Zhu <1078813195@qq.com>
2021-10-11 17:31:12 +0800

    EdgeWeightDigraph data strucutre

---
+++ Graph/DirectedEdge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmImplementation.Graph
+{
+    public class DirectedEdge
+    {
+        public int From { get; }
+        public int To { get; }
+        public double Weight { get; }
+
+
+        public DirectedEdge(int v, int w, double weight)
+        {
+            From = v;
+            To = w;
+            Weight = weight;
+        }
+    }
+}
---
+++ Graph/EdgeWeightedDigraph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicDataStrcture;
+
+namespace AlgorithmImplementation.Graph
+{
+    public class EdgeWeightedDigraph
+    {
+        public int V { get; }
+
+        public int E { get; protected set; }
+
+        protected Bag_L<DirectedEdge>[] _adjacencyList;
+
+        protected EdgeWeightedDigraph(int v)
+        {
+            V = v;
+            _adjacencyList = new Bag_L<DirectedEdge>[V];
+            for (int i = 0; i < V; i++)
+                _adjacencyList[i] = new Bag_L<DirectedEdge>();
+        }
+
+        public EdgeWeightedDigraph(int v, IEnumerable<(int, int, double)> edges) : this(v)
+        {
+            foreach (var edge in edges)
+                AddEdge(edge.Item1, edge.Item2, edge.Item3);
+        }
+
+        protected void AddEdge(int v, int w, double weight)
+        {
+            _adjacencyList[v].Add(new DirectedEdge(v, w, weight));
+            E++;
+        }
+
+        public IEnumerable<DirectedEdge> Edges()
+        {
+            foreach (var bag in _adjacencyList)
+                foreach (DirectedEdge edge in bag)
+                    yield return edge;
+        }
+
+        public IEnumerable<DirectedEdge> Adj(int v) => _adjacencyList[v];
+    }
+}
